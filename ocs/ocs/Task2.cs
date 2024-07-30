6a8417fd52a35bcfe45463f3e069b0696cca7bef
Tummkka <potapov.tim@gmail.com>
2024-07-30 19:23:29 +0300

    fix 4 issue

--- ocs/ocs/Task2.cs
+++ ocs/ocs/Task2.cs
@@ -14,77 +14,59 @@ namespace ocs
         static public string Solver2(string path)
         {
 
-            StreamReader reader = new StreamReader(path);
-            int kolvo = Convert.ToInt32(reader.ReadLine()); // колво наборов данных
-            string answer = "";
-            string number,data;
-            for(int i = 0; i < kolvo; i++) 
+            using (StreamReader reader = new StreamReader(path))
             {
-                
-                number = reader.ReadLine();
-                int top = 30, lowest = 15;  
-                for(int j = 0; j < Convert.ToInt32(number); j++ )
+                int kolvo = int.Parse(reader.ReadLine()); // колво наборов данных
+                string answer = "";
+                string number, data;
+                for (int i = 0; i < kolvo; i++)
                 {
-                    data = reader.ReadLine();
-                    string [] lines = data.Split();
-                    if (lowest > top)
+
+                    number = reader.ReadLine();
+                    int top = 30, lowest = 15;
+                    for (int j = 0; j < int.Parse(number); j++)
                     {
-                        answer += "-1";
-                        continue;
+                        data = reader.ReadLine();
+                        string[] lines = data.Split();
+                        int currentnumber = int.Parse(lines[1]);
+                        if (lines[0] == "<=")
+                        {
+
+                            if (currentnumber < top)
+                            {
+                                top = currentnumber;
+                                if (lowest > top) answer += "-1";
+                                else answer += lowest.ToString();
+                            }
+                            else
+                            {
+                                if (lowest > top) answer += "-1";
+                                else answer += lowest.ToString();
+                            }
+                        }
+                        else if (lines[0] == ">=")
+                        {
+
+                            if (currentnumber > lowest)
+                            {
+                                lowest = currentnumber;
+                                if (lowest > top) answer += "-1";
+                                else answer += lowest.ToString();
+
+                            }
+                            else
+                            {
+                                if (lowest > top) answer += "-1";
+                                else answer += lowest.ToString();
+
+                            }
+                        }
+
                     }
-                    if (lines[0] == "<=")
-                    {
-                        if (Convert.ToInt32(lines[1]) < lowest)
-                        {
-                            top = Convert.ToInt32(lines[1]);
-                            answer += "-1";
-                        }
-                        else if (Convert.ToInt32(lines[1]) == lowest)
-                        {
-                            top = Convert.ToInt32(lines[1]);
-                            answer += lines[1];
-                        }
-                        else if (Convert.ToInt32(lines[1]) > lowest)
-                        {
-                            if (Convert.ToInt32(lines[1]) >= top)
-                            {
-                                answer += lowest.ToString();
-                            }
-                            else if (Convert.ToInt32(lines[1]) < top)
-                            {
-                                top = Convert.ToInt32(lines[1]);
-                                answer += lowest.ToString();
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (Convert.ToInt32(lines[1]) < lowest)
-                        {
-                            answer += lowest.ToString();
-                        }
-                        else if (Convert.ToInt32(lines[1]) == lowest)
-                        {
-                            answer += lines[1];
-                        }
-                        else if (Convert.ToInt32(lines[1]) > lowest)
-                        {
-                            if (Convert.ToInt32(lines[1]) > top)
-                            {
-                                lowest = Convert.ToInt32(lines[1]);
-                                answer += "-1";
-                            }
-                            else if (Convert.ToInt32(lines[1]) <= top)
-                            {
-                                lowest = Convert.ToInt32(lines[1]);
-                                answer += lowest.ToString();
-                            }
-                        }
-                    }  
-                    
                 }
+
+                return answer;
             }
-            return answer;
         }
     }
 }
